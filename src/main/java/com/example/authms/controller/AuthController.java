1b68586b61f219d60b5aad88bc3df14e85111ba0
Javid Arkishiev <javidarkishiev@gmail.com>
2024-06-04 16:46:34 +0400

    add some changes

--- src/main/java/com/example/authms/controller/AuthController.java
+++ src/main/java/com/example/authms/controller/AuthController.java
@@ -15,7 +15,7 @@ import java.security.Principal;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("auth")
-@CrossOrigin(origins = "https://auth-ms-99dc7b517339.herokuapp.com")
+@CrossOrigin(origins = "https://auth-ms-99dc7b517339.herokuapp.com/**")
 public class AuthController {
     private final AuthService authService;
 
