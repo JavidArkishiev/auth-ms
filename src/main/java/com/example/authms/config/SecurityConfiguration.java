319b9094e7757b40b9cd2f7a42891d3e01fe6150
Javid Arkishiev <javidarkishiev@gmail.com>
2024-06-04 23:35:00 +0400

    add some changes

--- src/main/java/com/example/authms/config/SecurityConfiguration.java
+++ src/main/java/com/example/authms/config/SecurityConfiguration.java
@@ -85,7 +85,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://auth-ms-99dc7b517339.herokuapp.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Content-Type", "Authorization"));
         configuration.setExposedHeaders(List.of("Custom-Header1", "Custom-Header2"));
