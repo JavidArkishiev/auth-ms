aff61765b64225ae66de4467af49556c00a3d920
Javid Arkishiev <javidarkishiev@gmail.com>
2024-06-10 22:39:14 +0400

    add some changes

--- src/main/java/com/example/authms/controller/AuthController.java
+++ src/main/java/com/example/authms/controller/AuthController.java
@@ -5,12 +5,15 @@ import com.example.authms.dto.response.AuthResponse;
 import com.example.authms.exception.ExistEmailException;
 import com.example.authms.service.AuthService;
 import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.security.Principal;
 
 @RestController
@@ -59,6 +62,12 @@ public class AuthController {
         authService.resetPassword(resetPasswordRequest);
         return ResponseEntity.ok("Password has been reset successfully." +
                 " You can login a website with new password");
+    }
+
+    @PostMapping("refresh-token")
+    @ResponseStatus(HttpStatus.OK)
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        authService.refreshToken(request, response);
     }
 
 }
--- src/main/java/com/example/authms/service/AuthService.java
+++ src/main/java/com/example/authms/service/AuthService.java
@@ -7,14 +7,19 @@ import com.example.authms.exception.ExistEmailException;
 import com.example.authms.exception.AllException;
 import com.example.authms.mapper.UserMapper;
 import com.example.authms.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -191,5 +196,28 @@ public class AuthService {
     }
 
 
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        refreshToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(refreshToken);
+        if (userEmail != null) {
+            var user = userRepository.findByEmail(userEmail)
+                    .orElseThrow();
+            if (jwtService.isTokenValid(refreshToken, user)) {
+                var accessToken = jwtService.generateToken(user);
+
+                var authResponse = AuthResponse.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken)
+                        .build();
+                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+            }
+        }
+    }
 }
 
